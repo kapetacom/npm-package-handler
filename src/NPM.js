17cf9996fe4999ddd45939b58c90309738d55430
Jacob Wejendorp <jacob@wejendorp.dk>
2023-06-16 10:35:18 +0200

    chore: run prettier

--- src/NPM.js
+++ src/NPM.js
@@ -1,95 +1,93 @@
-const { spawnSync } = require("child_process");
-const Path = require("path");
-const FS = require("fs");
-const FSExtra = require("fs-extra");
-const OS = require("os");
+const { spawnSync } = require('child_process');
+const Path = require('path');
+const FS = require('fs');
+const FSExtra = require('fs-extra');
+const OS = require('os');
 class NPM {
-  /**
-   * Where the package should be located
-   * @param target
-   */
-  constructor(target) {
-    this._target = target;
-  }
-
-  install(packageName) {
-    const tmpFolder = OS.tmpdir() + "/npm-installer/" + packageName;
-    if (FS.existsSync(tmpFolder)) {
-      FSExtra.removeSync(tmpFolder);
+    /**
+     * Where the package should be located
+     * @param target
+     */
+    constructor(target) {
+        this._target = target;
     }
 
-    //Make sure parent dir is there
-    FSExtra.mkdirpSync(Path.dirname(this._target));
-    FSExtra.mkdirpSync(tmpFolder);
+    install(packageName) {
+        const tmpFolder = OS.tmpdir() + '/npm-installer/' + packageName;
+        if (FS.existsSync(tmpFolder)) {
+            FSExtra.removeSync(tmpFolder);
+        }
 
-    //Get rid of any npm environment variables - will confuse the process.
-    const filteredEnv = {};
-    Object.entries(process.env).forEach(([key, value]) => {
-      if (key.toLowerCase().startsWith("npm_")) {
-        return;
-      }
+        //Make sure parent dir is there
+        FSExtra.mkdirpSync(Path.dirname(this._target));
+        FSExtra.mkdirpSync(tmpFolder);
 
-      filteredEnv[key] = value;
-    });
+        //Get rid of any npm environment variables - will confuse the process.
+        const filteredEnv = {};
+        Object.entries(process.env).forEach(([key, value]) => {
+            if (key.toLowerCase().startsWith('npm_')) {
+                return;
+            }
 
-    //Install using npm
-    const escapedPath = tmpFolder.replace(/@/g, "\\@");
-    spawnSync(`npm pack --pack-destination ${escapedPath} ${packageName}`, {
-      stdio: "inherit",
-      shell: true,
-      env: filteredEnv,
-    });
+            filteredEnv[key] = value;
+        });
 
-    spawnSync(
-      `tar -xzf ${escapedPath}/${packageName
-        .replace(/@/g, "")
-        .replace("/", "-")}-*.tgz -C ${escapedPath}`,
-      {
-        stdio: "inherit",
-        shell: true,
-      }
-    );
+        //Install using npm
+        const escapedPath = tmpFolder.replace(/@/g, '\\@');
+        spawnSync(`npm pack --pack-destination ${escapedPath} ${packageName}`, {
+            stdio: 'inherit',
+            shell: true,
+            env: filteredEnv,
+        });
 
-    spawnSync(`npm i --omit=dev`, {
-      stdio: "inherit",
-      shell: true,
-      cwd: tmpFolder + "/package",
-      env: filteredEnv,
-    });
+        spawnSync(
+            `tar -xzf ${escapedPath}/${packageName.replace(/@/g, '').replace('/', '-')}-*.tgz -C ${escapedPath}`,
+            {
+                stdio: 'inherit',
+                shell: true,
+            }
+        );
 
-    //Move into place
-    FSExtra.moveSync(tmpFolder + "/package", this._target, { overwrite: true });
-    //Clean up
-    FSExtra.removeSync(tmpFolder);
-  }
+        spawnSync(`npm i --omit=dev`, {
+            stdio: 'inherit',
+            shell: true,
+            cwd: tmpFolder + '/package',
+            env: filteredEnv,
+        });
 
-  remove() {
-    try {
-      // use lstat to check if there is an existing symlink
-      // throws if nothing is there, but returns file stats even for invalid links
-      // we can't rely on fs.exists, since invalid symlinks return false
-      if (FS.lstatSync(this._target)) {
-        FSExtra.removeSync(this._target);
-      }
-    } catch (e) {}
-  }
-
-  upgrade(packageName) {
-    this.remove();
-    this.install(packageName);
-  }
-
-  link(folder) {
-    this.remove();
-
-    const packageFile = Path.join(folder, "package.json");
-    if (!FS.existsSync(packageFile)) {
-      throw new Error(`NPM module not found in folder: ${folder}`);
+        //Move into place
+        FSExtra.moveSync(tmpFolder + '/package', this._target, { overwrite: true });
+        //Clean up
+        FSExtra.removeSync(tmpFolder);
     }
 
-    FSExtra.mkdirpSync(Path.dirname(this._target));
-    FSExtra.createSymlinkSync(folder, this._target);
-  }
+    remove() {
+        try {
+            // use lstat to check if there is an existing symlink
+            // throws if nothing is there, but returns file stats even for invalid links
+            // we can't rely on fs.exists, since invalid symlinks return false
+            if (FS.lstatSync(this._target)) {
+                FSExtra.removeSync(this._target);
+            }
+        } catch (e) {}
+    }
+
+    upgrade(packageName) {
+        this.remove();
+        this.install(packageName);
+    }
+
+    link(folder) {
+        this.remove();
+
+        const packageFile = Path.join(folder, 'package.json');
+        if (!FS.existsSync(packageFile)) {
+            throw new Error(`NPM module not found in folder: ${folder}`);
+        }
+
+        FSExtra.mkdirpSync(Path.dirname(this._target));
+        FSExtra.createSymlinkSync(folder, this._target);
+    }
 }
 
 module.exports = NPM;
